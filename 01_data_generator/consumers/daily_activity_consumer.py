ef1959bca6ce13d024a9243a589f654fb0b6ab63
Javier Valverde <javier.valverde.dev@gmail.com>
2024-10-24 15:55:33 -0500

    feat: integrate AWS DynamoDB for daily activity consumer and handle float conversion

--- 01_data_generator/consumers/daily_activity_consumer.py
+++ 01_data_generator/consumers/daily_activity_consumer.py
@@ -1,6 +1,19 @@
 import logging
-from kafka import KafkaConsumer
 import os
+import boto3
+import json
+from dotenv import load_dotenv
+from kafka import KafkaConsumer
+from decimal import Decimal
+
+load_dotenv()  # take environment variables from .env.
+
+# Check AWS credentials are set up
+aws_access_key_id = os.getenv('AWS_ACCESS_KEY_ID')
+aws_secret_access_key = os.getenv('AWS_SECRET_ACCESS_KEY')
+
+if not aws_access_key_id or not aws_secret_access_key:
+    raise ValueError("AWS credentials are not set up.") # Raise an error if credentials are not set up
 
 # Set up logging
 logging.basicConfig(level=logging.INFO)
@@ -17,9 +30,30 @@ consumer = KafkaConsumer(
     auto_offset_reset='earliest',  # Start reading from the beginning
     enable_auto_commit=True,
     group_id=group_id,  # Set a group ID for managing offsets
-    value_deserializer=lambda x: x.decode('utf-8')
+    value_deserializer=lambda x: json.loads(x.decode('utf-8'))
 )
+
+def convert_floats_to_decimal(item):
+    """Recursively convert all float values in a dictionary to Decimal."""
+    if isinstance(item, list):
+        return [convert_floats_to_decimal(i) for i in item]
+    elif isinstance(item, dict):
+        return {k: convert_floats_to_decimal(v) for k, v in item.items()}
+    elif isinstance(item, float):
+        return Decimal(str(item))
+    return item
+
+
+# Initialize DynamoDB resource
+dynamodb = boto3.resource('dynamodb', region_name='us-east-2')
+table = dynamodb.Table('group2__health_tracker_daily_activity')
 
 # Read messages from the topic
 for message in consumer:
-    logging.info(f"Received message: {message.value}")
+    # logging.info(f"Received message: {value}")
+    value = convert_floats_to_decimal(message.value)
+    try:
+        response = table.put_item(Item=value)
+        print(f"Inserted item into DynamoDB: {response}")
+    except Exception as e:
+        print(f"Error inserting item into DynamoDB: {e}")
