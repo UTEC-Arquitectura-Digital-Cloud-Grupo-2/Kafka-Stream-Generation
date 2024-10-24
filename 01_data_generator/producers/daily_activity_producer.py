879c2ffb5cd7529461b541435651c64b68bdcc8d
Javier Valverde <javier.valverde.dev@gmail.com>
2024-10-24 12:36:06 -0500

    feat: added daily activity producer to send data to Kafka

---
+++ 01_data_generator/producers/daily_activity_producer.py
@@ -0,0 +1,34 @@
+from kafka import KafkaProducer
+import pandas as pd
+import json
+import logging
+import os
+
+# Set up logging
+logging.basicConfig(level=logging.INFO)
+
+bootstrap_servers = "localhost:9092"
+topic_name = "daily-activity"
+
+# Create a Kafka producer
+producer = KafkaProducer(
+    bootstrap_servers=[bootstrap_servers],  # Ensure this matches your Kafka bootstrap servers
+    value_serializer=lambda v: json.dumps(v).encode('utf-8')
+)
+
+# csv_filename
+current_dir = os.path.dirname(os.path.abspath(__file__))
+csv_filename = os.path.join(current_dir, '../simulated_data', 'daily_activity.csv')
+
+# Load the daily activity data
+daily_activity_data = pd.read_csv(csv_filename)
+
+# Send the daily activity data to the Kafka topic
+def send_daily_activity_data():
+    for _, row in daily_activity_data.iterrows():
+        producer.send(topic_name, row.to_dict())
+    producer.flush() # Ensure all messages are sent
+    logging.info(f"Daily activity data sent to Kafka topic: {topic_name}")
+    producer.close() # Close the connection
+
+send_daily_activity_data()
